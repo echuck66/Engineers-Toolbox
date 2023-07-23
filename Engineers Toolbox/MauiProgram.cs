f832395d2dd0235e564d1804227495ee578dc227
Charles Crawford <ccrawford@xpresscredit.net>
2023-07-23 11:32:23 -0400

    dev fix

--- Engineers Toolbox/MauiProgram.cs
+++ Engineers Toolbox/MauiProgram.cs
@@ -6,7 +6,8 @@ public static class MauiProgram
 {
 	public static MauiApp CreateMauiApp()
 	{
-		var builder = MauiApp.CreateBuilder().UseMauiApp<App>()
+		var builder = MauiApp.CreateBuilder()
+			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
